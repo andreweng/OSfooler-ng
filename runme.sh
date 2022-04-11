e0637e4fb51519a06ede56abb0c4985e501c7663
Andrew Eng <30492434+andreweng@users.noreply.github.com>
2022-04-11 11:45:05 -0400

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "Run container interactively: docker run -it <image> osfooler-ng -i eth0 -m \"Microsoft Windows 10\""
+echo "Run container interactively: docker run -it --cap-add=NET_ADMIN <image> osfooler-ng -i eth0 -m \"Microsoft Windows 10\""
